3c3567f6bed6c5664e218eeda0f0330f44070dbf
Gareth Fuller <gareth.fuller@clark.de>
2018-01-08 18:04:28 +0100

    :construction_worker: add fov changes for new method

--- ruckus/ARVC.swift
+++ ruckus/ARVC.swift
@@ -28,6 +28,10 @@ class ARVC: TimableController, TimableVCDelegate, ARSCNViewDelegate, PunchInTheH
     @IBOutlet weak var leftEyeView: UIView!
     @IBOutlet weak var rightEyeView: UIView!
     
+    @IBOutlet weak var imageViewLeft: UIImageView!
+    @IBOutlet weak var imageViewRight: UIImageView!
+    
+    
     let scene = ARScene.init(create: true)
     
     var gameOverlay: AROverlay?
@@ -50,6 +54,18 @@ class ARVC: TimableController, TimableVCDelegate, ARSCNViewDelegate, PunchInTheH
     var isPlaneSelected = false
     
     var started = false
+    
+    let eyeCamera : SCNCamera = SCNCamera()
+    
+    // Parametres
+    let interpupilaryDistance = 0.066 // This is the value for the distance between two pupils (in metres). The Interpupilary Distance (IPD).
+    let viewBackgroundColor : UIColor = UIColor.white
+    
+    // Set eyeFOV and cameraImageScale. Uncomment any of the below lines to change FOV.
+    //    let eyeFOV = 38.5; let cameraImageScale = 1.739; // (FOV: 38.5 ± 2.0) Brute-force estimate based on iPhone7+
+    let eyeFOV = 60; let cameraImageScale = 3.478; // Calculation based on iPhone7+ // <- Works ok for cheap mobile headsets. Rough guestimate.
+    //    let eyeFOV = 90; let cameraImageScale = 6; // (Scale: 6 ± 1.0) Very Rough Guestimate.
+    //    let eyeFOV = 120; let cameraImageScale = 8.756; // Rough Guestimate.
     
     required init?(coder aDecoder: NSCoder) {
 
@@ -75,7 +91,7 @@ class ARVC: TimableController, TimableVCDelegate, ARSCNViewDelegate, PunchInTheH
         
         let configuration = ARWorldTrackingConfiguration()
         configuration.planeDetection = .horizontal
-        leftEyeSceneAR.session.run(configuration, options: [
+        fullScreenARView.session.run(configuration, options: [
             ARSession.RunOptions.removeExistingAnchors,
             ARSession.RunOptions.resetTracking
         ])
@@ -85,10 +101,7 @@ class ARVC: TimableController, TimableVCDelegate, ARSCNViewDelegate, PunchInTheH
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        leftEyeView.isHidden = true
-//        leftEyeSceneAR.isHidden = true
-//        rightEyeSceneAR.isHidden = true
-//        rightEyeView.isHidden = true
+        
         fullScreenARView.isHidden = true
         
         // overlay configuration
@@ -190,16 +203,44 @@ class ARVC: TimableController, TimableVCDelegate, ARSCNViewDelegate, PunchInTheH
         rightEyeView.layer.cornerRadius = cornerSize
         rightEyeView.layer.masksToBounds = true
         
-//        fullScreenARView.scene = scene
+        fullScreenARView.scene = scene
         leftEyeSceneAR.scene = scene
         rightEyeSceneAR.scene = scene
         
+        fullScreenARView.delegate = self
+        
+        UIApplication.shared.isIdleTimerDisabled = true
+        
+        fullScreenARView.isHidden = true
+        
+
         // render delegate (in here for the VR stuff)
-        leftEyeSceneAR.delegate = self
+//        leftEyeSceneAR.delegate = self
         
         rightEyeSceneAR.isPlaying = true
+        leftEyeSceneAR.isPlaying = true
         
-        leftEyeSceneAR.automaticallyUpdatesLighting = true
+        self.view.backgroundColor = viewBackgroundColor
+        
+        ////////////////////////////////////////////////////////////////
+        // Create CAMERA
+        eyeCamera.zNear = 0.001
+        /*
+         Note:
+         - camera.projectionTransform was not used as it currently prevents the simplistic setting of .fieldOfView . The lack of metal, or lower-level calculations, is likely what is causing mild latency with the camera.
+         - .fieldOfView may refer to .yFov or a diagonal-fov.
+         - in a STEREOSCOPIC layout on iPhone7+, the fieldOfView of one eye by default, is closer to 38.5°, than the listed default of 60°
+         */
+        eyeCamera.fieldOfView = CGFloat(eyeFOV)
+        
+        ////////////////////////////////////////////////////////////////
+        // Setup ImageViews - for rendering Camera Image
+        self.imageViewLeft.clipsToBounds = true
+        self.imageViewLeft.contentMode = UIViewContentMode.center
+        self.imageViewRight.clipsToBounds = true
+        self.imageViewRight.contentMode = UIViewContentMode.center
+        
+//        leftEyeSceneAR.automaticallyUpdatesLighting = true
         
 //        leftEyeSceneAR.debugOptions = [.showConstraints, .showLightExtents, ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
     }
@@ -225,6 +266,7 @@ class ARVC: TimableController, TimableVCDelegate, ARSCNViewDelegate, PunchInTheH
             
             // overlay for both eyes
             if let overlay = gameOverlay {
+                fullScreenARView.overlaySKScene = overlay
                 leftEyeSceneAR.overlaySKScene = overlay
                 rightEyeSceneAR.overlaySKScene = overlay
             }
@@ -259,7 +301,7 @@ class ARVC: TimableController, TimableVCDelegate, ARSCNViewDelegate, PunchInTheH
         DispatchQueue.main.async {
             self.updateFrame()
             // call redraw on scene for agents etc
-            self.scene.update(updateAtTime: time)
+//            self.scene.update(updateAtTime: time)
         }
     }
     
@@ -324,23 +366,67 @@ class ARVC: TimableController, TimableVCDelegate, ARSCNViewDelegate, PunchInTheH
     
     func updateFrame() {
         
-        // Clone pointOfView for Second View
-        let pointOfView : SCNNode = (leftEyeSceneAR.pointOfView?.clone())!
-
+        // CREATE POINT OF VIEWS
+        let pointOfView : SCNNode = SCNNode()
+        pointOfView.transform = (fullScreenARView.pointOfView?.transform)!
+        pointOfView.scale = (fullScreenARView.pointOfView?.scale)!
+        // Create POV from Camera
+        pointOfView.camera = eyeCamera
+        
+        // Set PointOfView for SceneView-LeftEye
+        leftEyeSceneAR.pointOfView = pointOfView
+        
+        // Clone pointOfView for Right-Eye SceneView
+        let pointOfView2 : SCNNode = (leftEyeSceneAR.pointOfView?.clone())!
         // Determine Adjusted Position for Right Eye
         let orientation : SCNQuaternion = pointOfView.orientation
         let orientationQuaternion : GLKQuaternion = GLKQuaternionMake(orientation.x, orientation.y, orientation.z, orientation.w)
         let eyePos : GLKVector3 = GLKVector3Make(1.0, 0.0, 0.0)
         let rotatedEyePos : GLKVector3 = GLKQuaternionRotateVector3(orientationQuaternion, eyePos)
         let rotatedEyePosSCNV : SCNVector3 = SCNVector3Make(rotatedEyePos.x, rotatedEyePos.y, rotatedEyePos.z)
-
-        let mag : Float = 0.066 // This is the value for the distance between two pupils (in metres). The Interpupilary Distance (IPD).
-        pointOfView.position.x += rotatedEyePosSCNV.x * mag
-        pointOfView.position.y += rotatedEyePosSCNV.y * mag
-        pointOfView.position.z += rotatedEyePosSCNV.z * mag
-
-        // Set PointOfView for SecondView
-        rightEyeSceneAR.pointOfView = pointOfView
+        let mag : Float = Float(interpupilaryDistance)
+        pointOfView2.position.x += rotatedEyePosSCNV.x * mag
+        pointOfView2.position.y += rotatedEyePosSCNV.y * mag
+        pointOfView2.position.z += rotatedEyePosSCNV.z * mag
+        
+        // Set PointOfView for SceneView-RightEye
+        rightEyeSceneAR.pointOfView = pointOfView2
+        
+        ////////////////////////////////////////////
+        // RENDER CAMERA IMAGE
+        /*
+         Note:
+         - as camera.contentsTransform doesn't appear to affect the camera-image at the current time, we are re-rendering the image.
+         - for performance, this should be ideally be ported to metal
+         */
+        // Clear Original Camera-Image
+        leftEyeSceneAR.scene.background.contents = UIColor.clear // This sets a transparent scene bg for all sceneViews - as they're all rendering the same scene.
+        
+        // Read Camera-Image
+        let pixelBuffer : CVPixelBuffer? = fullScreenARView.session.currentFrame?.capturedImage
+        if pixelBuffer == nil { return }
+        let ciimage = CIImage(cvPixelBuffer: pixelBuffer!)
+        // Convert ciimage to cgimage, so uiimage can affect its orientation
+        let context = CIContext(options: nil)
+        let cgimage = context.createCGImage(ciimage, from: ciimage.extent)
+        
+        // Determine Camera-Image Scale
+        var scale_custom : CGFloat = 1.0
+        // let cameraImageSize : CGSize = CGSize(width: ciimage.extent.width, height: ciimage.extent.height) // 1280 x 720 on iPhone 7+
+        // let eyeViewSize : CGSize = CGSize(width: self.view.bounds.width / 2, height: self.view.bounds.height) // (736/2) x 414 on iPhone 7+
+        // let scale_aspectFill : CGFloat = cameraImageSize.height / eyeViewSize.height // 1.739 // fov = ~38.5 (guestimate on iPhone7+)
+        // let scale_aspectFit : CGFloat = cameraImageSize.width / eyeViewSize.width // 3.478 // fov = ~60
+        // scale_custom = 8.756 // (8.756) ~ appears close to 120° FOV - (guestimate on iPhone7+)
+        // scale_custom = 6 // (6±1) ~ appears close-ish to 90° FOV - (guestimate on iPhone7+)
+        scale_custom = CGFloat(cameraImageScale)
+        
+        // Determine Camera-Image Orientation
+        let imageOrientation : UIImageOrientation = (UIApplication.shared.statusBarOrientation == UIInterfaceOrientation.landscapeLeft) ? UIImageOrientation.down : UIImageOrientation.up
+        
+        // Display Camera-Image
+        let uiimage = UIImage(cgImage: cgimage!, scale: scale_custom, orientation: imageOrientation)
+        self.imageViewLeft.image = uiimage
+        self.imageViewRight.image = uiimage
 
     }
     
